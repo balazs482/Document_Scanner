179cb35d99463fc25af01fe17a44b6d3ec2d9382
Tóth Balázs <43994623+balazs482@users.noreply.github.com>
2020-09-29 02:45:06 +0200

    More variables moved to the beginning
    Easier to separate values that can be tweaked and ones that are certainly right

--- Document_Scanner/Document_Scanner.py
+++ Document_Scanner/Document_Scanner.py
@@ -2,11 +2,14 @@ import numpy as np
 import cv2
 
 IMAGE_PATH = 'test samples/sample_1.jpg'
+BLUR_VALUE = 1.4
+CANNY_LOWTHRESHOLD = 90 # default is 100
+CANNY_HIGHTRESHOLD = 360 # default is 200
 THRESHOLD_VALUE = 127
 
 # get image with blur and canny
 originalImage = cv2.imread(IMAGE_PATH, -1)
-img =  cv2.Canny(cv2.GaussianBlur(originalImage, (5, 5), 1.4), 90, 360) # default: lowThreshold = 100; highTreshold = 200
+img =  cv2.Canny(cv2.GaussianBlur(originalImage, (5, 5), BLUR_VALUE), CANNY_LOWTHRESHOLD, CANNY_HIGHTRESHOLD)
 
 # find contours
 ret, thresh = cv2.threshold(img, THRESHOLD_VALUE, 255, cv2.THRESH_BINARY)
