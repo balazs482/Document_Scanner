4452ca8ff3d855191932eccecb713063b8636c48
balazs482 <balazs482@gmail.com>
2020-12-01 11:12:43 +0100

    Constants moved to separate file

--- source/Document_Scanner.py
+++ source/Document_Scanner.py
@@ -2,30 +2,23 @@ import numpy as np
 import cv2
 import itertools
 
-IMAGE_PATH = 'test samples/sample_1.jpg'
-BLUR_VALUE = 10
-CANNY_LOWTHRESHOLD = 40 # default is 100
-CANNY_HIGHTRESHOLD = 300 # default is 200
-THRESHOLD_VALUE = 127
-ELLIPSE_COEFFICIENT = 0.08
-CORNER_DISTANCE_RATIO = 3
-PLOTTING_RATIO = 0.6
+import constants
 
 # get image with blur and canny
-originalImage = cv2.imread(IMAGE_PATH, -1)
-img =  cv2.Canny(cv2.GaussianBlur(originalImage, (5, 5), BLUR_VALUE), CANNY_LOWTHRESHOLD, CANNY_HIGHTRESHOLD)
+originalImage = cv2.imread(constants.IMAGE_PATH, -1)
+img =  cv2.Canny(cv2.GaussianBlur(originalImage, (5, 5), constants.BLUR_VALUE), constants.CANNY_LOWTHRESHOLD, constants.CANNY_HIGHTRESHOLD)
 HEIGHT, WIDTH = img.shape
 
 # find contours
-ret, thresh = cv2.threshold(img, THRESHOLD_VALUE, 255, cv2.THRESH_BINARY)
+ret, thresh = cv2.threshold(img, constants.THRESHOLD_VALUE, 255, cv2.THRESH_BINARY)
 contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
 # contour approximation
 contours = sorted(contours, key = cv2.contourArea, reverse = True)
 for i in contours:
     elip =  cv2.arcLength(i, True)
-    approx = cv2.approxPolyDP(i, ELLIPSE_COEFFICIENT * elip, True)
-    if len(approx) == 4: 
+    approx = cv2.approxPolyDP(i, constants.ELLIPSE_COEFFICIENT * elip, True)
+    if len(approx) == 4:
         doc = approx
         break
 doc = doc.reshape((4,2))
@@ -39,9 +32,9 @@ else:
     minDistance = min(HEIGHT, WIDTH)
     for pair in itertools.combinations(doc, r = 2):
         distance = np.linalg.norm(pair[0] - pair[1])
-        if (distance < minDistance): 
+        if (distance < minDistance):
             minDistance = distance
-    if minDistance < HEIGHT / CORNER_DISTANCE_RATIO:
+    if minDistance < HEIGHT / constants.CORNER_DISTANCE_RATIO:
         # error
         print('ERROR: Corners are too close together')
     else:
@@ -67,7 +60,7 @@ else:
         img = cv2.warpPerspective(originalImage, cv2.getPerspectiveTransform(corners, destinationPoints), (maxWidth, maxHeight))
 
         # plotting image
-        cv2.imshow('Frame', cv2.resize(img, (0, 0), fx = PLOTTING_RATIO, fy = PLOTTING_RATIO))
+        cv2.imshow('Frame', cv2.resize(img, (0, 0), fx = constants.PLOTTING_RATIO, fy = constants.PLOTTING_RATIO))
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
